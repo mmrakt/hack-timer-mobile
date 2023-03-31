97627f560ee6a9655feca9252a7fa0bc261f1265
mmrakt <mimuraakkun3@gmail.com>
2023-03-31 16:58:34 +0900

    add react-native-countdown-circle-timer

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json'
+  },
+  plugins: ['react'],
+  rules: {
+    '@typescript-eslint/no-misused-promises': 0,
+    '@typescript-eslint/strict-boolean-expressions': 0,
+    '@typescript-eslint/indent': 0,
+    '@typescript-eslint/no-floating-promises': 0,
+    '@typescript-eslint/no-unused-vars': 1,
+    '@typescript-eslint/consistent-type-definitions': 0,
+    '@typescript-eslint/prefer-ts-expect-error': 0,
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'multiline-ternary': 0,
+    'no-case-declarations': 0
+  },
+  ignorePatterns: []
+}
--- App.tsx
+++ App.tsx
@@ -1,13 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
+
+const UrgeWithPleasureComponent = () => (
+  <CountdownCircleTimer
+    isPlaying
+    duration={7}
+    colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+    colorsTime={[7, 5, 2, 0]}
+  >
+    {({ remainingTime }) => <Text>{remainingTime}</Text>}
+  </CountdownCircleTimer>
+)
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <UrgeWithPleasureComponent />
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +29,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})

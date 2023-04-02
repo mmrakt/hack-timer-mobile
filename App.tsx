be809613185d8eae06d54142b779421556492eb4
mmrakt <mimuraakkun3@gmail.com>
2023-04-02 14:26:47 +0900

    add nativewind

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 
 import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
 
@@ -16,19 +16,9 @@ const UrgeWithPleasureComponent = () => (
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
       <UrgeWithPleasureComponent />
       <StatusBar style="auto" />
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
